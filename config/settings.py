06f32a4679a223f35e4f77e0ec84ad8e172eedd2
hundred999-b <hundred@inesh.org>
2026-08-19 09:10:37 +0100

    Fix Telegram Mini App loading and orders

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-irbzp2a+l@4b%bnddzq$z%50w_x+7mqv7m%ecuqv4x2iv42yf$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["tgm-or6z.onrender.com", ".onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "tgm-or6z.onrender.com", ".onrender.com"]
 
 
 # Application definition
